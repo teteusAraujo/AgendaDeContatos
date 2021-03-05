58a5666ed3e9d1c98339121dc942a303644cfb2b
Mateus Araújo <mateusaraujo996@gmail.com>
2021-03-05 18:03:40 -0300

    Iniciando o Projeto de Agenda de Contatos

---
+++ src/main/java/com/mateus/contatos/AgendaDeContatosApplication.java
@@ -0,0 +1,13 @@
+package com.mateus.contatos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgendaDeContatosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgendaDeContatosApplication.class, args);
+	}
+
+}
